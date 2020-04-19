8bd1aac4a013891e93e8b4678d6ef25b637d683a
RIADAPLE <00032219@uca.edu.sv>
2020-04-19 16:01:24 -0600

    Añadiendo utilidad a empresa :v

--- Parcial/src/com/DARV/x00101519/Empresa.java
+++ Parcial/src/com/DARV/x00101519/Empresa.java
@@ -1,10 +1,13 @@
 package com.DARV.x00101519;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Empresa {
     private String nombre;
-    private ArrayList<Empleado> Planilla = new ArrayList<>();
+    private ArrayList<PlazaFija> Datos = new ArrayList<>();
+    private ArrayList<ServicioProfesional> Dato = new ArrayList<>();
+    Scanner info = new Scanner(System.in);
 
     public Empresa(String nombre) {
         this.nombre = nombre;
@@ -14,13 +17,70 @@ public class Empresa {
         return nombre;
     }
 
-    public ArrayList <Empleado> getPlanilla() {
-        return Planilla;
+    public ArrayList <PlazaFija> getPlanilla1() {
+        return Datos;
     }
+    public ArrayList <ServicioProfesional> getPlanilla2() {return Dato;}
 
     public void addEmplado( ) {
+        byte buf1 = 0;
+        printMenu1();
+        buf1 = info.nextByte(); info.nextLine();
+        switch(buf1){
+            case 1:
+                System.out.println("Digite el nombre del empleado:");
+                String name = info.nextLine();
+
+                String position = "Plaza Fija";
+
+                System.out.println("Digite su salario: ");
+                Double ingre = info.nextDouble(); info.nextLine();
+
+                System.out.println("Digite su extensión: ");
+                int ext = info.nextInt(); info.nextLine();
+
+                System.out.println("Digite su documento: ");
+                String docu = info.nextLine();
+
+                System.out.println("Digite su número de documento: ");
+                String num = info.nextLine();
+
+                Datos.add(new PlazaFija(name,position,ingre,docu,num,ext));
+                break;
+            case 2:
+                System.out.println("Digite el nombre del empleado:");
+                String name1 = info.nextLine();
+
+                String position1 = "Servicio Profesional";
+
+                System.out.println("Digite su salario: ");
+                Double ingre1 = info.nextDouble(); info.nextLine();
+
+                System.out.println("Digite sus meses de contrato: ");
+                int mes = info.nextInt(); info.nextLine();
+
+                System.out.println("Digite su documento: ");
+                String docu1 = info.nextLine();
+
+                System.out.println("Digite su número de documento: ");
+                String num1 = info.nextLine();
+
+                Dato.add(new ServicioProfesional(name1,position1,ingre1,docu1,num1,mes));
+                break;
+            default:
+                System.out.println("Opción errónea!");
+                break;
+        }
     }
 
-    public void quitEmplado( ) {
+    public void quitEmplado(String name) {
+        Datos.removeIf(s -> s.getNombre().equals(name));
+        Dato.removeIf(s -> s.getNombre().equals(name));
+    }
+    static void printMenu1(){
+        System.out.println("\n¿Qué puesto tiene el empleado que desea ingresar?");
+        System.out.println("1.Plaza fija");
+        System.out.println("2.Servicio profesional");
+        System.out.print("Su opción elegida es: ");
     }
 }
--- Parcial/src/com/DARV/x00101519/Main.java
+++ Parcial/src/com/DARV/x00101519/Main.java
@@ -12,76 +12,36 @@ public class Main {
         byte buf = 0;
         byte buf1 = 0;
         CalculadoraImpuestos calcu = new CalculadoraImpuestos(0,0,0);
+        System.out.println("Introduzca el nombre de la empresa");
+        String Empresita = info.nextLine();
+        Empresa Empres = new Empresa(Empresita);
 
         do {
+            System.out.println("----"+Empres.getNombre()+"----");
             printMenu();
             buf = info.nextByte();
             info.nextLine();
             switch (buf) {
                 case 1:
-                    printMenu1();
-                    buf1 = info.nextByte(); info.nextLine();
-                    switch(buf1){
-                        case 1:
-                            System.out.println("Digite el nombre del empleado:");
-                            String name = info.nextLine();
-
-                            String position = "Plaza Fija";
-
-                            System.out.println("Digite su salario: ");
-                            Double ingre = info.nextDouble(); info.nextLine();
-
-                            System.out.println("Digite su extensión: ");
-                            int ext = info.nextInt(); info.nextLine();
-
-                            System.out.println("Digite su documento: ");
-                            String docu = info.nextLine();
-
-                            System.out.println("Digite su número de documento: ");
-                            String num = info.nextLine();
-
-                            Datos.add(new PlazaFija(name,position,ingre,docu,num,ext));
-                            break;
-                        case 2:
-                            System.out.println("Digite el nombre del empleado:");
-                            String name1 = info.nextLine();
-
-                            String position1 = "Servicio Profesional";
-
-                            System.out.println("Digite su salario: ");
-                            Double ingre1 = info.nextDouble(); info.nextLine();
-
-                            System.out.println("Digite sus meses de contrato: ");
-                            int mes = info.nextInt(); info.nextLine();
-
-                            System.out.println("Digite su documento: ");
-                            String docu1 = info.nextLine();
-
-                            System.out.println("Digite su número de documento: ");
-                            String num1 = info.nextLine();
-
-                            Dato.add(new ServicioProfesional(name1,position1,ingre1,docu1,num1,mes));
-                            break;
-                        default:
-                            System.out.println("Opción errónea!");
-                            break;
-                    }
-
+                    Empres.addEmplado();
                     break;
                 case 2:
                     System.out.println("Introduzca el nombre del empleado que quiere eliminar:");
                     String name = info.nextLine();
-                    Datos.removeIf(s -> s.getNombre().equals(name));
-                    Dato.removeIf(s -> s.getNombre().equals(name));
+                    Empres.quitEmplado(name);
                     System.out.println("Eliminando...");
                     break;
                 case 3:
+                    Datos = Empres.getPlanilla1();
+                    Dato = Empres.getPlanilla2();
                     Datos.forEach(PlazaFija -> System.out.println(PlazaFija.toString()));
                     Dato.forEach(ServicioProfesional -> System.out.println(ServicioProfesional.toString()));
                     break;
                 case 4:
                     System.out.println("Introduzca el nombre del empleado al cual le quiere calcular el sueldo: ");
                     String name1 = info.nextLine();
+                    Datos = Empres.getPlanilla1();
+                    Dato = Empres.getPlanilla2();
                     Datos.forEach(s -> {
                         if (name1.equalsIgnoreCase(s.getNombre())) {
                             System.out.println("$"+calcu.calcularPago(s));
@@ -104,7 +64,6 @@ public class Main {
         } while (buf != 0);
     }
     static void printMenu () {
-        System.out.println("----Menu---");
         System.out.println("1. Desea agregar un empleado:");
         System.out.println("2. Digite el empleado a eliminar:");
         System.out.println("3. Desea ver la lista de empleados.");
@@ -114,10 +73,4 @@ public class Main {
         System.out.print("Su opcion elegida es: ");
     }
 
-    static void printMenu1(){
-        System.out.println("\n¿Qué puesto tiene el empleado que desea ingresar?");
-        System.out.println("1.Plaza fija");
-        System.out.println("2.Servicio profesional");
-        System.out.print("Su opción elegida es: ");
-    }
 }
