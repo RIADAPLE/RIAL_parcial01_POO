1cafb9658666cc4a755c361b04248efa9ee5fb69
RIADAPLE <00032219@uca.edu.sv>
2020-04-19 23:10:32 -0600

    Subiendo el catch final

--- Parcial/src/com/DARV/x00101519/Main.java
+++ Parcial/src/com/DARV/x00101519/Main.java
@@ -10,66 +10,73 @@ public class Main {
         Scanner info = new Scanner(System.in);
         ArrayList<PlazaFija> Datos = new ArrayList<>();
         ArrayList<ServicioProfesional> Dato = new ArrayList<>();
-        byte buf = 0;
-        byte buf1 = 0;
         CalculadoraImpuestos calcu = new CalculadoraImpuestos(0,0,0);
         System.out.println("Introduzca el nombre de la empresa");
         String Empresita = info.nextLine();
         Empresa Empres = new Empresa(Empresita);
+        boolean continuar = true;
 
-        do {try{
-            System.out.println("----"+Empres.getNombre()+"----");
-            printMenu();
-            buf = info.nextByte();
-            info.nextLine();}
-        catch (InputMismatchException ex) {
-            System.out.println("Debe ingresar obligatoriamente un número entero.");
-        }
-            switch (buf) {
-                case 1:
-                    Empres.addEmplado();
-                    break;
-                case 2:
-                    System.out.println("Introduzca el nombre del empleado que quiere eliminar:");
-                    String name = info.nextLine();
-                    Empres.quitEmplado(name);
-                    System.out.println("Eliminando...");
-                    break;
-                case 3:
-                    Datos = Empres.getPlanilla1();
-                    Dato = Empres.getPlanilla2();
-                    Datos.forEach(PlazaFija -> {System.out.println(PlazaFija.toString());
-                    PlazaFija.mostrardocu();
-                    });
-                    Dato.forEach(ServicioProfesional -> {System.out.println(ServicioProfesional.toString());
-                    ServicioProfesional.mostrardocu();});
-                    break;
-                case 4:
-                    System.out.println("Introduzca el nombre del empleado al cual le quiere calcular el sueldo: ");
-                    String name1 = info.nextLine();
-                    Datos = Empres.getPlanilla1();
-                    Dato = Empres.getPlanilla2();
-                    Datos.forEach(s -> {
-                        if (name1.equalsIgnoreCase(s.getNombre())) {
-                            System.out.println("$"+calcu.calcularPago(s));
-                        }
-                    });
-                    Dato.forEach(s -> {
-                        if (name1.equalsIgnoreCase(s.getNombre())) {
-                            System.out.println("$"+calcu.calcularPago(s));
-                        }
-                    });
-                    break;
-                case 5:
-                    System.out.println("Mostrando totales: ");
-                    calcu.mostrarTotales();
-                    break;
-                default:
-                    System.out.println("Opción errónea!");
-                    break;
+
+        do {
+            try {
+                System.out.println("----" + Empres.getNombre() + "----");
+                printMenu();
+                String buf = info.nextLine();
+                byte buf1 = Byte.parseByte(buf);
+
+                switch (buf1) {
+                    case 1:
+                        Empres.addEmplado();
+                        break;
+                    case 2:
+                        System.out.println("Introduzca el nombre del empleado que quiere eliminar:");
+                        String name = info.nextLine();
+                        Empres.quitEmplado(name);
+                        System.out.println("Eliminando...");
+                        break;
+                    case 3:
+                        Datos = Empres.getPlanilla1();
+                        Dato = Empres.getPlanilla2();
+                        Datos.forEach(PlazaFija -> {
+                            System.out.println(PlazaFija.toString());
+                            PlazaFija.mostrardocu();
+                        });
+                        Dato.forEach(ServicioProfesional -> {
+                            System.out.println(ServicioProfesional.toString());
+                            ServicioProfesional.mostrardocu();
+                        });
+                        break;
+                    case 4:
+                        System.out.println("Introduzca el nombre del empleado al cual le quiere calcular el sueldo: ");
+                        String name1 = info.nextLine();
+                        Datos = Empres.getPlanilla1();
+                        Dato = Empres.getPlanilla2();
+                        Datos.forEach(s -> {
+                            if (name1.equalsIgnoreCase(s.getNombre())) {
+                                System.out.println("$" + calcu.calcularPago(s));
+                            }
+                        });
+                        Dato.forEach(s -> {
+                            if (name1.equalsIgnoreCase(s.getNombre())) {
+                                System.out.println("$" + calcu.calcularPago(s));
+                            }
+                        });
+                        break;
+                    case 5:
+                        System.out.println("Mostrando totales: ");
+                        calcu.mostrarTotales();
+                        break;
+                    case 0:
+                        continuar = false;
+                        break;
+                    default:
+                        System.out.println("Opción errónea!");
+                        break;
                 }
-
-        } while (buf != 0);
+            }catch (NumberFormatException exc) {
+                System.out.println("La opción debe ser un número");
+            }
+        } while (continuar);
     }
     static void printMenu () {
         System.out.println("1. Desea agregar un empleado:");
@@ -80,5 +87,7 @@ public class Main {
         System.out.println("0. Salir.");
         System.out.print("Su opcion elegida es: ");
     }
+
+
 
 }
